3e895c194380dfc9e505fecfe118e294ee090bb7
J. Junquera <javier@junquera.xyz>
2019-11-17 12:53:50 +0100

    No funcionan las variables de entorno en el cgi...

--- init_pg.sh
+++ init_pg.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER shellshock WITH password 'shellshock';
-    CREATE DATABASE shellshock;
-    GRANT ALL PRIVILEGES ON DATABASE shellshock TO shellshock;
-EOSQL
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+#     CREATE USER shellshock WITH password 'shellshock';
+#     CREATE DATABASE shellshock;
+#     GRANT ALL PRIVILEGES ON DATABASE shellshock TO shellshock;
+# EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "shellshock" <<-EOSQL
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "shellshock" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "POSTGRES_DB" <<-EOSQL
   create table users (username varchar, password varchar);
   insert into users (username, password) VALUES ('pepito', 'perez');
   insert into users (username, password) VALUES ('juanito', 'lopez');
